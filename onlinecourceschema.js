974e103545042df73cbb898da5b947361a66b161
amardeepshinde <50072077+amardeepshinde@users.noreply.github.com>
2020-04-14 18:04:23 +0530

    Add files via upload

---
+++ onlinecourceschema.js
@@ -0,0 +1,41 @@
+db.cources.insert({
+ "cources": [ 
+    {
+        "cource_id": "1",
+        "modelname": "ruby",
+         "qustions": [ { "qustion1":[ {"a": 20,
+                                      "b": 30,
+                                      "c": 40
+                                      } ],
+                        "qustion2":[ {"a": 20,
+                                       "b": 30,
+                                       "c": 40
+                                      } ]  
+                       }],
+                       
+                       
+        "cource_id": "2",
+        "modelname": "rails",
+         "qustions": [ { "qustion1": [{"a": 20,
+                                      "b": 30,
+                                      "c": 40
+                                      } ],
+                        "qustion2": [ {"a": 20,
+                                       "b": 30,
+                                       "c": 40
+                                      } ]  
+                       }],
+                       
+             "users": 123
+                       
+                   }],
+                   
+              
+               
+               
+               
+               
+     "users": [  { "name" : "xyz",
+                 "userid" : 123
+     }]
+ })
